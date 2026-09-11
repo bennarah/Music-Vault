39e64152989ca699553fb6bfa65de924f56f5ba6
bennarah <bennarah@csu.fullerton.edu>
2026-09-10 20:22:58 -0700

    Configure frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Recommendations from './pages/Recommendations/Recommendations';
+
 function App() {
   return (
-    <div>
-      <h1>Music Vault</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/recommendations"
+        element={<Recommendations />}
+      />
+    </Routes>
   );
 }
 
