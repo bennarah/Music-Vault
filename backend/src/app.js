c8538f2589a8126efc77458b0a767e0fb749064a
bennarah <bennarah@csu.fullerton.edu>
2026-09-24 21:27:55 -0700

    Implement create preference endpoint

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-//Configures Express and connects routes
+// Configures Express and connects routes
 
 const express = require("express");
 const cors = require("cors");
@@ -7,15 +7,18 @@ require("dotenv").config();
 
 const healthRoutes = require("./routes/healthRoutes");
 const spotifyTestRoutes = require("./routes/spotifyTest");
+const preferenceRoutes = require("./routes/preferenceRoutes");
 
 const app = express();
 
 app.use(cors({
     origin: "http://localhost:3000"
 }));
+
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
 app.use("/api/spotify-test", spotifyTestRoutes);
+app.use("/api/preferences", preferenceRoutes);
 
 module.exports = app;
